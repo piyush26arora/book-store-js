496179012e05c884e999b997927289889faac5c2
Piyush Arora <83628525+piyush26arora@users.noreply.github.com>
2023-01-31 21:15:12 +0530

    Initial commit

---
+++ assets/utils/constants/constant.js
@@ -0,0 +1,51 @@
+const types = {
+    TEXT: "Text" ,
+    NUMBER: "Number" ,
+    DATE: "Date" , 
+    BOOLEAN: "Boolean"
+}
+
+const {TEXT, NUMBER, DATE, BOOLEAN} = types
+
+const inputs = [
+    {
+        id: 1,
+        key: "ISBN",
+        type: NUMBER
+    },
+    {
+        id: 2,
+        key: "Author Name",
+        type: TEXT
+    },
+    {
+        id: 3,
+        key: "Book Name",
+        type: TEXT
+    },
+    {
+        id: 4,
+        key: "Page Number",
+        type: NUMBER
+    },
+    {
+        id: 5,
+        key: "Price",
+        type: NUMBER
+    },
+    {
+        id: 6,
+        key: "Year of Publishing",
+        type: DATE
+    },
+    {
+        id: 7,
+        key: "Discount",
+        type: NUMBER
+    },
+    {
+        id: 8,
+        key: "In Stock",
+        type: TEXT
+    }
+]
